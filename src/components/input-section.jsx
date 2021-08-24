e8653cf0e551bd245f8e60a21ab4c3bed6bd5262
Adnan Saleem <adnan13893@gmail.com>
2021-08-24 11:48:14 +0500

    error message for empty input

--- src/components/input-section.jsx
+++ src/components/input-section.jsx
@@ -1,15 +1,34 @@
-import React, {useState} from "react";
+import React, {useState, useEffect} from "react";
 import "../css/input-section.css";
 import {FormControl, Button} from "react-bootstrap";
 
 const InputSection = ({onSubmit, initialColor}) => {
   const [color, setColor] = useState(initialColor);
+  const [showError, setShowError] = useState(false);
+  useEffect(() => {
+    let input = document.getElementById("color-input");
+    if (showError) {
+      input.style.borderColor = "red";
+    } else {
+      input.style.borderColor = "#ced4da";
+    }
+  }, [showError]);
+
   const handleChange = (e) => {
     setColor(e.target.value);
   };
   const handleKeyDown = (event) => {
     if (event.key === "Enter") {
+      handleSubmit();
+    }
+  };
+
+  const handleSubmit = () => {
+    setShowError(false);
+    if (color !== "") {
       onSubmit(color);
+    } else {
+      setShowError(true);
     }
   };
 
@@ -25,9 +44,10 @@ const InputSection = ({onSubmit, initialColor}) => {
         onChange={handleChange}
         onKeyDown={handleKeyDown}
       />
-      <Button id="btn-color-submit" onClick={() => onSubmit(color)}>
+      <Button id="btn-color-submit" onClick={handleSubmit}>
         Submit
       </Button>
+      {showError && <p id="input-error">Please enter a valid value</p>}
     </>
   );
 };
